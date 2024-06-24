68075c65e8fea21c30485ee59d8263ea530a8ac7
Lucas Balmès <lucas.balmes@etu.uca.fr>
2024-06-24 10:51:49 +0200

    Mise en place des init de la SDL

--- src/headers/sdl_common.h
+++ src/headers/sdl_common.h
@@ -1,1 +1,21 @@
 #define SDL_COMMON_H
+#include <SDL2/SDL.h>
+
+struct ui_s
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+    SDL_Texture *front_card_textures[7];
+    SDL_Texture *back_card_texture;
+    SDL_Texture *player_textures[5];
+    SDL_Texture *interface_textures[4];
+    int screen_w;
+    int screen_h;
+    int board_size;
+    bool in_pause;
+    bool program_on;
+};
+typedef struct ui_s ui_t;
+
+ui_t *create_ui();
--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include "headers/sdl_common.h"
 
 int main(int argc, char const *argv[])
 {
-    
+    (void)argc;
+    (void)argv;
+
+    ui_t *ui = create_ui();
+    // create_game_state
+    // create_input
+
+    while (ui->program_on)
+    {
+        //refresh_input(ui, input);
+        //draw(ui, game)
+    }
+
+    //free_input(input);
+    //free_game_state(game);
+    //free_ui(ui);
+
     return 0;
 }
