013273d0ed2a3ff84a57e6b751349d4d8303039a
SAIM <thanhhen2002@gmail.com>
2024-06-24 10:30:49 +0200

    implémentation des structures du jeu et les fonctions de la pile

--- src/headers/struct.h
+++ src/headers/struct.h
@@ -1,1 +1,36 @@
 #define STRUCT_H
+
+
+// Structure de pile
+struct stack
+{
+    int value;
+    struct stack *next;
+};
+typedef stack stack_t;
+struct player
+{
+    int tank[4];          // Carte tank de joueur
+    int score;            // Score du joueur
+    int last_scored_card; // Valeur de la dernière carte jouée
+};
+typedef player player_t;
+
+struct card
+{
+    int back[3]; // Dos de la carte
+    int face;    // Face de la carte
+};
+typedef card card_t;
+
+struct game
+{
+    int players[3];    // Nombre de joueurs
+    int player_action; // Joueur actif
+    int win[4];        // 0 si joueur n'a pas gagné, 1 si joueur a gagné
+    stack *draw_pile;  // Pile de pioche
+};
+typedef game game_t;
+
+
+
---
+++ src/struct.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers/struct.h"
+
+
+/*************************** Fonctions de la structure de données pile *****************************/
+/**
+ * @brief Création d'une pile vide  
+ * 
+ * @return stack_t* 
+ */
+stack_t * stackCreate() 
+{
+	return NULL;
+}
+
+
+/**
+ * @brief Vérifie si la pile est vide
+ * 
+ * @param stack la pile à vérifier
+ * @return int 
+ */
+int stack_is_empty(stack_t *stack)
+{
+    return stack == NULL;
+}
+
+
+/**
+ * @brief Ajoute un élément à la pile
+ * 
+ * @param stack la pile
+ * @param value la valeur à ajouter
+ * @return stack_t* 
+ */
+stack_t * stack_push(stack_t *stack, int value)
+{
+    stack_t *new_stack = malloc(sizeof(stack_t));
+    if (new_stack == NULL)
+    {
+        fprintf(stderr, "Erreur d'allocation de mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+    new_stack->value = value;
+    new_stack->next = stack;
+    return new_stack;
+}
+
+
+/**
+ * @brief Retourne le sommet de la pile
+ * 
+ * @param stack la pile
+ * @return int 
+ 
+ */
+int stack_top(stack_t *stack)
+{
+    if (stack_is_empty(stack))
+    {
+        fprintf(stderr, "Erreur: pile vide\n");
+        exit(EXIT_FAILURE);
+    }
+    return stack->value;
+}
+
+
+/**
+ * @brief Retire le sommet de la pile
+ * 
+ * @param stack la pile
+ */
+stack_t * stack_pop(stack_t *stack)
+{
+    if (stack_is_empty(stack))
+    {
+        fprintf(stderr, "Erreur: pile vide\n");
+        exit(EXIT_FAILURE);
+    }
+    stack_t *new_stack = stack->next;
+    free(stack);
+    return new_stack;
+}
+
+
+/**
+ * @brief Retourne la taille de la pile
+ * 
+ * @param stack la pile
+ * @return int 
+ */
+int stack_size(stack_t *stack)
+{
+    int size = 0;
+    while (!stack_is_empty(stack))
+    {
+        size++;
+        stack = stack->next;
+    }
+    return size;
+}
+
+/**
+ * @brief Libère la mémoire allouée à la pile
+ * 
+ * @param stack la pile
+ */
+void stack_free(stack_t *stack)
+{
+    while (!stack_is_empty(stack))
+    {
+        stack = stack_pop(stack);
+    }
+}
+
+/*************************** Fonctions de la structure de données pile *****************************/
