d1ba260435219b4cf90ab03f409517cf579a8b96
Lucas Balmès <lucas.balmes@etu.uca.fr>
2024-06-24 18:02:05 +0200

    Remise en page struct.c

--- src/struct.c
+++ src/struct.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "headers/struct.h"
-
 
 /*************************** Fonctions de la structure de données pile *****************************/
 /**
- * @brief Création d'une pile vide  
- * 
- * @return stack_t* 
+ * @brief Création d'une pile vide
+ *
+ * @return stack_t*
  */
-stack_t * stack_create() 
+stack_t *stack_create()
 {
-	return NULL;
+    return NULL;
 }
-
 
 /**
  * @brief Vérifie si la pile est vide
- * 
+ *
  * @param stack la pile à vérifier
- * @return int 
+ * @return int
  */
-int stack_is_empty(stack_t *stack)
+bool stack_is_empty(stack_t *stack)
 {
     return stack == NULL;
 }
 
-
 /**
  * @brief Ajoute un élément à la pile
- * 
+ *
  * @param stack la pile
  * @param face la face de la carte
  * @param back le dos de la carte
- * @return stack_t* 
+ * @return stack_t*
  */
-stack_t * stack_push(stack_t *stack, int face, int back[2])
+stack_t *stack_push(stack_t *stack, int face, int back[2])
 {
     stack_t *new_stack = malloc(sizeof(stack_t));
     if (new_stack == NULL)
@@ -44,21 +42,19 @@ stack_t * stack_push(stack_t *stack, int face, int back[2])
         exit(EXIT_FAILURE);
     }
     new_stack->card.face = face;
-    for (int i = 0 ; i < 2 ; i++)
+    for (int i = 0; i < 2; i++)
     {
         new_stack->card.back[i] = back[i];
     }
-    new_stack->next= stack;
+    new_stack->next = stack;
     return new_stack;
 }
 
-
 /**
  * @brief Retourne la face du sommet de la pile
- * 
+ *
  * @param stack la pile
- * @return int 
- 
+ * @return int
  */
 int stack_top(stack_t *stack)
 {
@@ -70,13 +66,12 @@ int stack_top(stack_t *stack)
     return stack->card.face;
 }
 
-
 /**
  * @brief Retire le sommet de la pile
- * 
+ *
  * @param stack la pile
  */
-stack_t * stack_pop(stack_t *stack)
+stack_t *stack_pop(stack_t *stack)
 {
     if (stack_is_empty(stack))
     {
@@ -88,12 +83,11 @@ stack_t * stack_pop(stack_t *stack)
     return new_stack;
 }
 
-
 /**
  * @brief Retourne la taille de la pile
- * 
+ *
  * @param stack la pile
- * @return int 
+ * @return int
  */
 int stack_size(stack_t *stack)
 {
@@ -108,7 +102,7 @@ int stack_size(stack_t *stack)
 
 /**
  * @brief Libère la mémoire allouée à la pile
- * 
+ *
  * @param stack la pile
  */
 void stack_free(stack_t *stack)
