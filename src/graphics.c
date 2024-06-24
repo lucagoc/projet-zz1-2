eaa8741ef64ce2cc74377062f8eb98cc1e61cbda
Lucas Balmès <lucas.balmes@etu.uca.fr>
2024-06-24 19:54:36 +0200

    Affichage de l'emplacement des cartes de chaque joueur

--- src/graphics.c
+++ src/graphics.c
@@ -20,18 +20,41 @@ void draw_background(ui_t *ui)
 void draw_players(ui_t *ui, game_t *game)
 {
     SDL_SetRenderDrawColor(ui->renderer, 255, 255, 255, 255);
+
+    int size_length = ui->screen_w / 2 - 90;
+    int size_height = 200;
+
     if (game->players[0] != NULL)
     {
-        int size_x = 130;
-        int size_y = 80;
-
         SDL_SetRenderDrawColor(ui->renderer, 255, 0, 0, 255);
-        SDL_Rect player_background = {ui->screen_w/2 - size_x/2, ui->screen_h - size_y, size_x, size_y};
+        SDL_Rect player_background = {0, ui->screen_h - size_height, size_length, size_height};
         SDL_RenderFillRect(ui->renderer, &player_background);
 
         // SDL_Rect player_avatar_rect = {ui->screen_w - 40, ui->screen_h - 80, 40, 40};
         // SDL_RenderCopy(ui->renderer, ui->player_textures[0], NULL, &player_avatar_rect); Pour l'affichage d'un éventuel avatar.
     }
+    
+    if(game->players[1] != NULL)
+    {
+        SDL_SetRenderDrawColor(ui->renderer, 0, 255, 0, 255);
+        SDL_Rect player_background = {ui->screen_w - size_length, 0, size_length, size_height};
+        SDL_RenderFillRect(ui->renderer, &player_background);
+    }
+
+    if(game->players[2] != NULL)
+    {
+        SDL_SetRenderDrawColor(ui->renderer, 0, 0, 255, 255);
+        SDL_Rect player_background = {0, 0, size_length, size_height};
+        SDL_RenderFillRect(ui->renderer, &player_background);
+    }
+
+    if(game->players[3] != NULL)
+    {
+        SDL_SetRenderDrawColor(ui->renderer, 255, 255, 0, 255);
+        SDL_Rect player_background = {ui->screen_w - size_length, ui->screen_h - size_height, size_length, size_height};
+        SDL_RenderFillRect(ui->renderer, &player_background);
+    }
+    
 }
 
 // Affiche UNE carte.
@@ -47,13 +70,13 @@ void draw_draw_cards(ui_t *ui, game_t *game, int x, int y)
     for (int i = 0; i < 3; i++) // 3 = nombre d'élément derrière la carte.
     {
 
-        SDL_Rect draw_indicator_rect = {x + i * 30 + 10, y + 45 + i * 67, FLAG_WIDTH / 2, FLAG_HEIGHT / 2};
+        SDL_Rect draw_indicator_rect = {x + i * 20 + 7, y + 30 + i * 45, FLAG_WIDTH / 3, FLAG_HEIGHT / 3};
         SDL_RenderCopy(ui->renderer, ui->back_flag_textures[oui_debug], NULL, &draw_indicator_rect);
         oui_debug += 1;
     }
 
     // Affiche la carte
-    SDL_Rect draw_pile_rect = {x, y, CARD_WIDTH / 2, CARD_HEIGHT / 2};
+    SDL_Rect draw_pile_rect = {x, y, CARD_WIDTH / 3, CARD_HEIGHT / 3};
     SDL_RenderCopy(ui->renderer, ui->back_card_texture, NULL, &draw_pile_rect);
 }
 
@@ -67,7 +90,7 @@ void draw_cards(ui_t *ui, game_t *game)
             // draw_card
         }
     }
-    draw_draw_cards(ui, game, ui->screen_w / 2 - (CARD_WIDTH / 4), ui->screen_h / 2 - (CARD_HEIGHT / 4));
+    draw_draw_cards(ui, game, ui->screen_w / 2 - (CARD_WIDTH / 6), ui->screen_h / 2 - (CARD_HEIGHT / 6));
 }
 
 void draw_buttons(ui_t *ui, game_t *game)
@@ -77,7 +100,7 @@ void draw_buttons(ui_t *ui, game_t *game)
 
 void draw_logo(ui_t *ui)
 {
-    SDL_Rect draw_logo_rect = {10, 10, 200, 200};
+    SDL_Rect draw_logo_rect = {10, ui->screen_h/2-100, 200, 200};
     SDL_RenderCopy(ui->renderer, ui->interface_textures[0], NULL, &draw_logo_rect);
 }
 
